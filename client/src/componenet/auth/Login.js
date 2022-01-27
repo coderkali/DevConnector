06076203c837f379f8b1446da896b24a62f62782
coderkali <coderkali@gmail.com>
2022-01-26 22:47:18 -0500

    remove the unwanted code

--- client/src/componenet/auth/Login.js
+++ client/src/componenet/auth/Login.js
@@ -6,10 +6,8 @@ export const Login = () => {
     const [formData, setFormData] = useState({
         email:'',
         password:'',
-
     });
     const { email, password } = formData;
-    
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
 
     const onSubmit = async e => {
